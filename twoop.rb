c2bf066465b78fb6d8eec943ce6e867605fe4d8e
Won-Min Chang <wonminchang@gmail.com>
2015-09-06 01:47:56 +0000

    twoop without the replay loop

--- twoop.rb
+++ twoop.rb
@@ -1,6 +1,20 @@
-puts "What is your name?"
-input = gets.chomp
-puts "Hello, #{name}"
+# puts "Hi, Player1. What is your name?"
+# player1_name = gets.chomp
+# puts "Hello, #{player1_name}"
 
-# class Player 
-#   def 
+# puts "Hi, Player2. What is your name?"
+# player2_name = gets.chomp
+# puts "Hello, #{player2_name}"
+
+class Player 
+  def initialize(name, lives, points)
+    @name = name
+    @lives = 3
+    @points = 0
+  end
+
+  def player_status
+  puts "#{player1_name}, you have #{@lives} and #{points}"
+  end 
+end  
+
